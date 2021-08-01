61037ff48341afe22830776227c9f8a1f1042f3e
Subham Sharma <subham.pandit@hotmail.com>
2021-08-01 22:18:54 +0530

    initial commit

---
+++ Multiply.java
@@ -0,0 +1,8 @@
+public class Multiply{
+    public static void main(String[] args){
+        int a = 3;
+        int b = 5;
+        System.out.println(a+b);
+        System.out.println(a*b);
+    }
+}
